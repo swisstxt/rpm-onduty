733138ace4202214f9c1c1a73d36cb821359ebfc
Nik Wolfgramm <niwo@users.noreply.github.com>
2014-11-11 08:48:37 +0100

    Update puma.rb

--- SOURCES/puma.rb
+++ SOURCES/puma.rb
@@ -1,4 +1,6 @@
-bind            'unix://./tmp/puma.sock'
+directory       '/srv/onduty'
+bind            'tcp://0.0.0.0:9292'
+#bind            'unix://./tmp/puma.sock'
 pidfile         './tmp/puma.pid'
 state_path      './tmp/puma.state'
 stdout_redirect './log/puma.log', './log/puma.log', true
